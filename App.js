9312006866e21ded3ea2165e5cf2fefb6f7ae6ee
MSCincis <mariasole.cincis@gmail.com>
2024-03-21 13:22:02 +0100

    feature: add stack navigator

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import AddTaskScreen from './src/screens/AddTaskScreen';
 
+const Stack = createStackNavigator();
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddTask" component={AddTaskScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+/*export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world!
@@ -18,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
+
+
+
+
