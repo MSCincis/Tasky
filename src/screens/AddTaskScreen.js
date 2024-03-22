c5189ec8f2143615774981b0b0cbec8121de904d
MSCincis <mariasole.cincis@gmail.com>
2024-03-22 13:56:04 +0100

    feature: finally implemented UI and logic to create a new task , add it to the list and refresh the list to see all the created tasks

--- src/screens/AddTaskScreen.js
+++ src/screens/AddTaskScreen.js
@@ -1,34 +1,77 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import databaseService from '../database/DatabaseService'; 
 
 const AddTaskScreen = ({ navigation }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
-  const handleAddTask = () => {
-    // Add task to the database
-    navigation.goBack();
+  const handleAddTask = async () => {
+    try {
+      await databaseService.addTask(title, description);
+      navigation.goBack();
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
   };
 
   return (
-    <View>
-      <Text>Add New Task</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Title:</Text>
       <TextInput
-        placeholder="Title"
+        style={styles.input}
         value={title}
         onChangeText={setTitle}
       />
+      <Text style={styles.label}>Description:</Text>
       <TextInput
-        placeholder="Description"
+        style={styles.input}
         value={description}
         onChangeText={setDescription}
+        multiline
       />
-      <Button
-        title="Add Task"
+      <TouchableOpacity
+        style={styles.Button}
         onPress={handleAddTask}
-      />
+      >
+        <Text style={styles.ButtonText}>Add Task</Text>
+      </TouchableOpacity>
+  
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  input: {
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  Button: {
+    backgroundColor: '#007bff',
+    padding: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginVertical: 5,
+    marginHorizontal: 16,
+   
+  },
+  ButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
 export default AddTaskScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import { FlatList, StyleSheet, View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import databaseService from '../database/DatabaseService'; // Adjust the import path as necessary
 
 const HomeScreen = ({ navigation }) => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    databaseService.initDB(); // Initialize DB and create table if not exists
+
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const fetchedTasks = await databaseService.getAllTasks();
+      setTasks(fetchedTasks);
+    } catch (error) {
+      console.error("Failed to fetch tasks:", error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Task List</Text>
-      {/* List of tasks will be displayed here */}
-      <Button
-        title="Add Task"
-        onPress={() => navigation.navigate('AddTask')}
+    <View style={styles.container}>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.taskItem}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
       />
+      <TouchableOpacity
+        style={styles.Button}
+        onPress={() => navigation.navigate('AddTask')}
+      >
+        <Text style={styles.ButtonText}>New Task</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.Button}
+        onPress={fetchTasks}
+      >
+        <Text style={styles.ButtonText}>Refresh List</Text>
+      </TouchableOpacity>
+     
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+  taskItem: {
+    backgroundColor: '#f9c2ff',
+    borderRadius: 5,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  Button: {
+    backgroundColor: '#007bff',
+    padding: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginVertical: 5,
+    marginHorizontal: 16,
+   
+  },
+  ButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
 export default HomeScreen;
+
